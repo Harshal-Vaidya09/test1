f9c1917537baae086b6f3dcb07866c7aef8b87b5
Harshal-Vaidya09 <vaidyaharshal09@gmail.com>
2022-07-25 10:41:03 +0530

    first commit

---
+++ string1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+
+    cout<<"enter a string ";
+
+    
+    getline(cin,s);
+
+    cout<<"you have entered --> ";
+    cout<<s<<endl;
+ 
+    return 0;
+}
